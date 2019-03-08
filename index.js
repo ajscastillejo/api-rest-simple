d71f4c11a6262c6144cdfecdfde7c519e2fc1129
adrigt00 <adriangomez@MacBook-Pro-de-Adrian.local>
2019-03-08 12:00:33 +0100

    elimina y cre usuarios vacios

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose     = require('mongoose');
+const router = express.Router();
+const mongo = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
+const assert = require('assert');
 
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use( bodyParser.urlencoded({ extended: false}))
+app.use( bodyParser.urlencoded({ extended: true}))
 app.use( bodyParser.json())
 
 mongoose.Promise = Promise;
@@ -39,7 +43,7 @@ app.get('/users/:id1', (req, res) => {
     if (err) return handleError(err);
     
 		console.log(usuario.id, usuario.first_name, usuario.last_name, usuario.avatar);
-        res.send( 'hola ' + usuario.first_name + ' ' + usuario.last_name);
+        res.send(usuario);
 		
   });
 	
@@ -53,6 +57,34 @@ app.get('/users', (req, res) => {
   });
 	
 })
+
+app.post('/create', function (req, res) {
+  var newUser = new User();
+    newUser.first_name = req.body.first_name;
+    newUser.last_name = req.body.last_name;
+    newUser.id = req.body.id;
+    newUser.avatar = req.body.avatar;
+    newUser.save(function (err) {
+        if (err)  {
+          res.json(err);
+          res.status(500); 
+        }
+        res.json({
+            message: 'Nuevo usuario creado!!!',
+            data: newUser
+        });
+        res.status(201); 
+    });
+});
+
+app.delete('/delete/:id1', function (req, res) {
+  var id1 = req.params.id1;
+  User.findOneAndRemove({ 'id': id1 }, function (err, usuario) {
+    if (err) return handleError(err);
+  res.status(204);
+});
+  res.redirect('/users');
+});
 
 
 
